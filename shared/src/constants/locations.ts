b7d51061352fbee1fce6f210f2c5556589d2e1c0
Solomon-Asefa <soloasefa6603@gmail.com>
2026-08-27 00:02:30 +0300

    chore(shared): restrict region scope strictly to Addis Ababa for stage 1 release

--- shared/src/constants/locations.ts
+++ shared/src/constants/locations.ts
@@ -1,33 +1,20 @@
-export const ETHIOPIAN_REGIONS = [
-  'Addis Ababa',
-  'Afar',
-  'Amhara',
-  'Benishangul-Gumuz',
-  'Dire Dawa',
-  'Gambela',
-  'Harari',
-  'Oromia',
-  'Sidama',
-  'Somali',
-  'South Ethiopia',
-  'South West Ethiopia Peoples',
-  'Tigray'
-] as const;
+// Stage 1: Strictly focused on Addis Ababa
+export const ETHIOPIAN_REGIONS = ["Addis Ababa"] as const;
 
 export type EthiopianRegion = (typeof ETHIOPIAN_REGIONS)[number];
 
 export const ADDIS_ABABA_SUBCITIES = [
-  'Addis Ketema',
-  'Akaky Kaliti',
-  'Arada',
-  'Bole',
-  'Gullele',
-  'Kirkos',
-  'Kolfe Keraniyo',
-  'Lideta',
-  'Nifas Silk-Lafto',
-  'Lemi Kura',
-  'Yeka'
+  "Addis Ketema",
+  "Akaky Kaliti",
+  "Arada",
+  "Bole",
+  "Gullele",
+  "Kirkos",
+  "Kolfe Keraniyo",
+  "Lideta",
+  "Nifas Silk-Lafto",
+  "Lemi Kura",
+  "Yeka",
 ] as const;
 
 export type AddisAbabaSubCity = (typeof ADDIS_ABABA_SUBCITIES)[number];
